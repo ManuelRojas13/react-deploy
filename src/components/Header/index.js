a745461e914efa27264b2e8a0f17fa7af65348d1
ManuelARD13 <115947544+ManuelARD13@users.noreply.github.com>
2022-12-25 17:22:09 -0300

    adding files trial

---
+++ src/components/Header/index.js
@@ -0,0 +1,24 @@
+import React from "react"
+import "./Header.css"
+import { RiArrowDownCircleLine } from "react-icons/ri"
+
+function Header() {
+    return (
+        <header id="home">
+            <div className="titleContainer">
+                <h4>Manuel</h4>
+                <h1>ROJAS</h1>
+                <p>Freelancer Front-end Developer</p>
+                <a href = "#projectsSection">
+                <input type={"button"} value="My Projects" className="button"/>
+                </a>
+                
+            </div>
+            <a href="#personalInfo"><RiArrowDownCircleLine className="scrollDownButton"/></a>
+                
+        </header>
+    )
+}
+
+export {Header}
+
---
+++ src/components/Nav/index.js
@@ -0,0 +1,39 @@
+import React from "react"
+import "./Nav.css"
+import { GoMarkGithub } from "react-icons/go";
+import { BsLinkedin } from "react-icons/bs";
+import { BsWhatsapp } from "react-icons/bs";
+import { SiGmail } from "react-icons/si";
+
+function Nav() {
+    return (
+        <nav>
+            <div className="logo">
+                <p className="logo-provitional">MR</p>
+                <p className="logo-sub-tittle">Front-end<br />Developer</p>
+            </div>
+            <div className="menu">
+                <p><a href="#pageTop">HOME</a></p>
+                <p><a href="#projectsSection">PORTFOLIO</a></p>
+                <p><a href="#contactSection">CONTACT</a></p>
+                <p><a href="#personalInfo">ABOUT</a></p>
+            </div>
+            <div className="social-media">
+                <a href="https://github.com/ManuelARD13" target="_blank" rel="noopener noreferrer">
+                    <GoMarkGithub className="social-media-icons" />
+                </a>
+                <a href="https://www.linkedin.com/in/manuel-alejandro-rojas-duran-464a12192/" target="_blank" rel="noopener noreferrer">
+                    <BsLinkedin className="social-media-icons" />
+                </a>
+                <a href="mailto:duranalejandro661@gmail.com" target="_blank" rel="noopener noreferrer">
+                    <SiGmail className="social-media-icons" />
+                </a>
+                <a href="https://wa.me/5491154881218" target="_blank" rel="noopener noreferrer">
+                    <BsWhatsapp className="social-media-icons" />
+                </a>
+            </div>
+        </nav>
+    )
+}
+
+export { Nav }
